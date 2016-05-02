f037e2dbcec6a70f74562328d66173e7a851d638
Alan Tsai <alan@alantsai.net>
2016-05-02 16:01:55 +0800

    增加登出按鈕

---
+++ AspNetWebForm/src/Aspnet Webform CASClient Sample/Logout.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DotNetCasClient;
+
+namespace Aspnet.Webform.CASClient.Sample
+{
+    public partial class Logout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CasAuthentication.ClearAuthCookie();
+
+            Response.Redirect("Default.aspx");
+        }
+    }
+}
